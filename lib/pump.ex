5eff0f949b8aff5ed420736eff4f562b8e86f6fa
Jan <janek.ziniewicz@gmail.com>
2018-06-08 16:51:38 +0200

    Refactor handling messages

--- lib/pump.ex
+++ lib/pump.ex
@@ -6,6 +6,8 @@ defmodule Pump do
   alias Pump.Metrics.{OSMon, VMMemory, VMStatistics, VMSystemInfo}
   alias Pump.InfluxDBWriter
 
+  def send_stats(pid, stats), do: send(pid, {:send_stats, stats})
+
   def start_link(env) do
     GenServer.start_link(__MODULE__, env)
   end
@@ -29,26 +31,31 @@ defmodule Pump do
     tags = Keyword.merge([device_id: device_id], custom_tags)
     state = {http_client, tags, send_interval}
 
-    Process.send(self(), :send_stats, [])
+    Process.send(self(), :gather_and_send_stats, [])
 
     {:ok, state}
   end
 
-  def handle_info(:send_stats, {http_client, tags, send_interval} = state) do
+  def handle_info(:gather_and_send_stats, {_, _, send_interval} = state) do
     stats = gather_stats()
-    send_stats(http_client, stats, tags)
+    send_stats(self(), stats)
 
-    Process.send_after(self(), :send_stats, send_interval)
+    Process.send_after(self(), :gather_and_send_stats, send_interval)
 
     {:noreply, state}
   end
 
-  def gather_stats() do
+  def handle_info({:send_stats, stats}, {http_client, tags, _} = state) do
+    send_stats(http_client, stats, tags)
+    {:noreply, state}
+  end
+
+  defp gather_stats() do
     VMMemory.metrics() ++ VMStatistics.metrics() ++ VMSystemInfo.metrics() ++ OSMon.metrics()
   end
 
-  def send_stats(http_client, stats, tags) do
-    case Pump.InfluxDBWriter.write(http_client, stats, tags) do
+  defp send_stats(http_client, stats, tags) do
+    case InfluxDBWriter.write(http_client, stats, tags) do
       {:error, error} ->
         Logger.warn("Error sending measurements to InfluxDB. Reason: #{inspect(error)}")
         {:error, error}
